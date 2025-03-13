1d802097b89027f21e6690828397d1f4aa9ff0c1
Vishal Ahirwar <vishalchoudhry0091@gmail.com>
2025-03-14 05:28:23 +0530

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,3 @@
-// TODO : remove goto
 #include <iostream>
 #include <boost-process-example-projectconfig.h>
 #include <boost/process.hpp>
@@ -6,14 +5,18 @@
 #include <fmt/color.h>
 #include <fmt/core.h>
 #include <fstream>
-
+std::ofstream out("log.txt");
 char lc[] = {"/\\-+"};
 namespace fs = std::filesystem;
 
 void log(const std::string_view &msg, const char &end = '\n')
 {
-    fmt::print(fmt::emphasis::bold | fmt::fg(fmt::color::white), "{}{:<5}: ", end, "Info");
+    fmt::print(fmt::emphasis::bold | fmt::fg(fmt::color::white), "{:<5}: ", "Info");
     fmt::print(fmt::emphasis::faint | fmt::fg(fmt::color::ghost_white), "{}{}", msg, end);
+};
+void logFile(const std::string_view &msg)
+{
+    out << msg << "\n";
 };
 
 int main(int argc, char *argv[])
@@ -24,22 +27,32 @@ int main(int argc, char *argv[])
         if (!fs::exists("build"))
         {
         build:
-            boost::process::ipstream ss;
+            boost::process::ipstream ss_out, ss_err; // Create separate streams for output and error
             std::stringstream stream;
-            boost::process::child c(boost::process::search_path("cmake"), "--preset=windows", "-Bbuild/debug", boost::process::std_out > ss, boost::process::std_err > ss);
-            std::string line{};
-            while (c.running() && std::getline(ss, line))
+            boost::process::child c(boost::process::search_path("cmake"), "--preset=windows", "-Bbuild/debug",
+                                    boost::process::std_out > ss_out, boost::process::std_err > ss_err); // Redirect std_err too
+
+            std::string line_out{}, line_err{};
+            while (c.running() && (std::getline(ss_out, line_out) || std::getline(ss_err, line_err)))
             {
                 log(std::format("Generating Config...{}", lc[rand() % strlen(lc)]), '\r');
-                stream << line << "\n";
+                if (!line_out.empty())
+                {
+                    logFile(line_out);
+                }
+                if (!line_err.empty())
+                {
+                    logFile(line_err);
+                }
             }
+
+            log("");
             c.wait();
+
             if (c.exit_code() != 0)
             {
-                std::ofstream out{"build/cmake-log.txt"};
-                out << stream.rdbuf();
-                out.close();
-                log(std::format("Generating Config failed read cmake-log.txt for more info"));
+
+                log(std::format("Generating Config failed. Read log.txt for more info"));
                 return 1;
             }
             goto compile;
@@ -48,28 +61,34 @@ int main(int argc, char *argv[])
         {
         compile:
 
-            boost::process::ipstream ss;
-            boost::process::child c(boost::process::search_path("cmake"), "--build", "build/debug", boost::process::std_out > ss);
-            std::string line{};
+            boost::process::ipstream ss_out, ss_err; // Separate streams for output and error
+            boost::process::child c(boost::process::search_path("cmake"), "--build", "build/debug",
+                                    boost::process::std_out > ss_out, boost::process::std_err > ss_err);
+
+            std::string line_out{}, line_err{};
             std::stringstream stream;
-            while (c.running() && std::getline(ss, line))
+            while (c.running() && (std::getline(ss_out, line_out) || std::getline(ss_err, line_err)))
             {
                 log(std::format("Compiling Project...{}", lc[rand() % strlen(lc)]), '\r');
-                stream << line << "\n";
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                if (!line_out.empty())
+                {
+                    logFile(line_out);
+                }
+                if (!line_err.empty())
+                {
+                    logFile(line_err);
+                }
             }
             c.wait();
+            log("");
             if (c.exit_code() != 0)
             {
-                std::ofstream out{"build/compile-log.txt"};
-                out<<stream.rdbuf();
-                out.close();
-                log("build failed read build/compile-log.txt for more info");
+                log("Build failed. Read log.txt for more info.");
                 return 1;
             }
             else
             {
-                log("build success");
+                log("Build success!");
                 if (argc >= 2)
                 {
                     if (std::string(argv[1]) == "--run")
